95a5f55ae6a52c2c88366584b013f179741d7556
Ananta Gupta <58319462+Ananta-Gupta@users.noreply.github.com>
2020-12-20 15:23:27 +0530

    First Commit

---
+++ Adventure Island Board Game/Assets/Scripts/GameControl.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    private static GameObject whoWinText, player1MoveText, player2MoveText;
+
+    private static GameObject player1, player2;
+
+    public static int diceSideThrown = 0;
+    public static int player1StaertWayPoint = 0;
+    public static int player2StaertWayPoint = 0;
+
+    public static bool gameOver = false;
+
+    void Start()
+    {
+        whoWinText = GameObject.Find("WhoWinText");
+        player1MoveText = GameObject.Find("Player1MoveText");
+        player2MoveText = GameObject.Find("Player2MoveText");
+
+        player1 = GameObject.Find("Player1");
+        player2 = GameObject.Find("Player2");
+
+        player1.GetComponent<FollowThePath>().moveAllowed = false;
+        player2.GetComponent<FollowThePath>().moveAllowed = false;
+
+        whoWinText.gameObject.SetActive(false);
+        player1MoveText.gameObject.SetActive(true);
+        player2MoveText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(player1.GetComponent<FollowThePath>().waypointIndex >
+            player1StaertWayPoint + diceSideThrown)
+        {
+            player1.GetComponent<FollowThePath>().moveAllowed = false;
+            player1MoveText.gameObject.SetActive(false);
+            player2MoveText.gameObject.SetActive(true);
+            player1StaertWayPoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
+        }
+
+        if (player2.GetComponent<FollowThePath>().waypointIndex >
+            player2StaertWayPoint + diceSideThrown)
+        {
+            player2.GetComponent<FollowThePath>().moveAllowed = false;
+            player2MoveText.gameObject.SetActive(false);
+            player1MoveText.gameObject.SetActive(true);
+            player2StaertWayPoint = player1.GetComponent<FollowThePath>().waypointIndex - 1;
+        }
+
+        if (player1.GetComponent<FollowThePath>().waypointIndex ==
+            player1.GetComponent<FollowThePath>().waypoints.Length)
+        {
+            whoWinText.gameObject.SetActive(true);
+            whoWinText.GetComponent<Text>().text = "Player 2 Wins";
+            gameOver = true;
+        }
+
+        if (player2.GetComponent<FollowThePath>().waypointIndex ==
+            player2.GetComponent<FollowThePath>().waypoints.Length)
+        {
+            whoWinText.gameObject.SetActive(true);
+            player1MoveText.gameObject.SetActive(false);
+            player2MoveText.gameObject.SetActive(false);
+            whoWinText.GetComponent<Text>().text = "Player 2 Wins";
+            gameOver = true;
+        }
+    }
+
+    public static void MovePlayer(int playerToMove)
+    {
+        switch (playerToMove)
+        {
+            case 1:
+                player1.GetComponent<FollowThePath>().moveAllowed = true;
+                break;
+            case 2:
+                player2.GetComponent<FollowThePath>().moveAllowed = true;
+                break;
+        }
+    }
+}
